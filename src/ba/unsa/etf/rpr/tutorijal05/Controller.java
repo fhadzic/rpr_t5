3d85063cdbdc1cd2c3e8800b7fc5379c6bd8be7b
fhadzic <fhadzic1@etf.unsa.ba>
2018-11-14 23:10:34 +0100

    first commit

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,80 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
+    public TextField inputField;
+    public Button buttonPercent;
+    public Button buttonDivide;
+    public Button buttonMultiple;
+    public Button buttonDecrease;
+    public Button buttonIncrease;
+    public Button buttonComma;
+    public Button buttonResult;
+    public Button button0;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+
+    public void buttonPercentClick(ActionEvent actionEvent) {
+
+    }
+    public void buttonDivideClick(ActionEvent actionEvent) {
+
+    }
+    public void buttonMultipleClick(ActionEvent actionEvent) {
+
+    }
+    public void buttonDecreaseClick(ActionEvent actionEvent) {
+
+    }
+    public void buttonIncreaseClick(ActionEvent actionEvent) {
+
+    }
+
+    public void buttonCommaClick(ActionEvent actionEvent) {
+
+    }
+    public void buttonResultClick(ActionEvent actionEvent) {
+
+    }
+    public void button0Click(ActionEvent actionEvent) {
+
+    }
+    public void button1Click(ActionEvent actionEvent) {
+
+    }
+    public void button2Click(ActionEvent actionEvent) {
+
+    }
+    public void button3Click(ActionEvent actionEvent) {
+
+    }
+    public void button4Click(ActionEvent actionEvent) {
+
+    }
+    public void button5Click(ActionEvent actionEvent) {
+
+    }
+    public void button6Click(ActionEvent actionEvent) {
+
+    }
+    public void button7Click(ActionEvent actionEvent) {
+
+    }
+    public void button8Click(ActionEvent actionEvent) {
+
+    }
+    public void button9Click(ActionEvent actionEvent) {
+
+    }
 }

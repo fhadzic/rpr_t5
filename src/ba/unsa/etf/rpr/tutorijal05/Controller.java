fa3196729839ce6ab46f0849c50c5414c1d662b4
fhadzic <fhadzic1@etf.unsa.ba>
2018-11-15 02:09:45 +0100

    Pisao Button metode. Dodao CSS.

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -25,56 +25,168 @@ public class Controller {
     public Button button8;
     public Button button9;
 
-    public void buttonPercentClick(ActionEvent actionEvent) {
+    private double firstNumber = 0;
+    private double secondNumber = 0;
+    private String method;
 
+    public void buttonPercentClick(ActionEvent actionEvent) {
+        firstNumber = Double.parseDouble(inputField.getText());
+        method = "%";
     }
+
     public void buttonDivideClick(ActionEvent actionEvent) {
 
     }
+
     public void buttonMultipleClick(ActionEvent actionEvent) {
 
     }
+
     public void buttonDecreaseClick(ActionEvent actionEvent) {
 
     }
-    public void buttonIncreaseClick(ActionEvent actionEvent) {
 
+    public void buttonIncreaseClick(ActionEvent actionEvent) {
+        firstNumber = Double.parseDouble(inputField.getText());
+        method = "+";
     }
 
     public void buttonCommaClick(ActionEvent actionEvent) {
 
     }
+
     public void buttonResultClick(ActionEvent actionEvent) {
+        secondNumber = Double.parseDouble(inputField.getText());
 
+        if (method.equals("%")) {
+            inputField.setText(String.valueOf(firstNumber % secondNumber));
+        } else if (method.equals("/")) {
+            inputField.setText(String.valueOf(firstNumber / secondNumber));
+        } else if(method.equals("+")){
+            inputField.setText(String.valueOf(firstNumber + secondNumber));
+        }
     }
+
     public void button0Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button1Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+            firstNumber += 1;
+        } else {
+            firstNumber = 1;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button2Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+            firstNumber += 2;
+        } else {
+            firstNumber = 2;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button3Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+            firstNumber += 3;
+        } else {
+            firstNumber = 3;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button4Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
+
+        String newInput = "";
+        if (input != 0) {
+            newInput += input + "4";
+            firstNumber = Double.parseDouble(newInput);
+            inputField.setText(String.valueOf(firstNumber));
+        } else {
+            firstNumber = 4;
+            inputField.setText(String.valueOf((int) firstNumber));
+        }
+
 
     }
+
     public void button5Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+            firstNumber += 5;
+        } else {
+            firstNumber = 5;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button6Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+            firstNumber += 6;
+        } else {
+            firstNumber = 6;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button7Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (input != 0) {
+            firstNumber = input * 10;
+            firstNumber += 7;
+        } else {
+            firstNumber = 7;
+        }
+        inputField.setText(String.valueOf(firstNumber));
     }
+
     public void button8Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
 
+        if (firstNumber != 0) {
+            firstNumber = input * 10;
+            firstNumber += 8;
+        }  else {
+            firstNumber = 8;
+        }
+
+        inputField.setText(String.valueOf(firstNumber));
     }
-    public void button9Click(ActionEvent actionEvent) {
 
+    public void button9Click(ActionEvent actionEvent) {
+        double input = Double.parseDouble(inputField.getText());
+
+        if (firstNumber != 0) {
+            firstNumber = input * 10;
+            firstNumber += 9;
+        }  else {
+            firstNumber = 9;
+        }
+
+        inputField.setText(String.valueOf(firstNumber));
     }
 }
